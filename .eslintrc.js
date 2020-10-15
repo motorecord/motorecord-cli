e3d812e187ac2f01cdacf529d062eb2a9bbd8a37
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-15 16:09:12 +0800

    引入mint-ui和eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        "plugin:vue/essential", "eslint:recommended"
+    ],
+    "rules": {
+        'no-console': 'off'
+    }
+};
