249c9488cf76233cf7fea3262a7e767d07f32909
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-16 00:46:15 +0800

    添加主布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import HelloWorld from '@/components/HelloWorld'
+import layout from '@/views/layout'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/hello',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/',
+      name: 'layout',
+      component: layout
     }
   ]
 })
