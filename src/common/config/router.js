141a72fa15dfdf49041862a42a86e7c478b32dfc
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-20 15:07:22 +0800

    路由component封装

--- src/common/config/router.js
+++ src/common/config/router.js
@@ -2,12 +2,12 @@ let routes = [
     {
         path: '/',
         name: 'layout',
-        component: () => import('../../views/layout.vue'),
+        component: 'layout',
         children: [
             {
                 path: '/index',
                 name: 'index',
-                component: () => import('../../views/index/index.vue')
+                component: 'index/index'
             }
         ]
     },
@@ -17,4 +17,24 @@ let routes = [
     }
 ]
 
-export default routes
+// 自动生成路由
+function createRoute(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (!arr[i].component) return arr;
+        // 自动生成component
+        let componentFun = import(`../../views/${arr[i].component}.vue`);
+        arr[i].component = () => componentFun;
+        if (arr[i].children && arr[i].children.length > 0) {
+            createRoute(arr[i].children);
+        }
+    }
+}
+
+// 获取路由信息
+function getRoutes() {
+    // 自动生成路由
+    createRoute(routes);
+    return routes;
+}
+
+export default getRoutes()
