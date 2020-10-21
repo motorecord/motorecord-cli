c4271b2fb88e23bc76a7859b0b38c9effa6e74f1
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-21 18:30:14 +0800

    登录页面

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,43 +1,63 @@
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="col-12 col-sm-10 col-md-7 col-lg-5 m-auto pt-5" style="height: 500px">
-				<div class="card mt-5">
-					<div class="card-header bg-white">
-						<h3 class="text-center mb-0 text-secondary">UNI-ADMIN</h3>
-					</div>
-					<div class="card-body">
-						<el-form ref="form" :model="form">
-							<el-form-item>
-								<el-input v-model="form.username" size="medium" placeholder="请输入用户名"></el-input>
-							</el-form-item>
-							<el-form-item>
-								<el-input v-model="form.password"  size="medium" placeholder="请输入密码" 
-								type="password">
-								</el-input>	
-							</el-form-item>
-							<el-form-item>
-								<el-button type="primary" size="medium" class="w-100">登陆</el-button>
-							</el-form-item>
-						</el-form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-10 col-md-7 col-lg-5 m-auto pt-5" style="height: 500px">
+                <div class="card mt-5">
+                    <div class="card-header bg-white">
+                        <h3 class="text-center mb-0 text-secondary">Motorecord</h3>
+                    </div>
+                    <div class="card-body">
+                        <el-form ref="ruleform" :rules="rules" :model="form">
+                            <el-form-item prop="username">
+                                <el-input v-model="form.username" size="medium" placeholder="请输入用户名"></el-input>
+                            </el-form-item>
+                            <el-form-item prop="password">
+                                <el-input v-model="form.password"  size="medium" placeholder="请输入密码" 
+                                type="password">
+                                </el-input>    
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary" size="medium" class="w-100" @click="submit">登陆</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-	export default{
-		data() {
-			return {
-				form: {
-					username:"",
-					password:""
-				}
-			}
-		},
-	}
+    export default{
+        data() {
+            return {
+                form: {
+                    username:"",
+                    password:""
+                },
+                rules:{
+                    username:[
+                        {required:true,message:"请输入用户名",trigger:'blur'},
+                    ],
+                    password:[
+                        {required:true,message:"请输入密码",trigger:"blur"}
+                    ]
+                }
+            }
+        },
+        methods:{
+            submit(){
+                console.log(this.$refs);
+                this.$refs.ruleform.validate((e)=>{
+                    //e为表单验证结果 
+                    if(!e) return;
+                    //验证通过，提交表单 TODO
+                    //跳转页面
+                    this.$router.push({name:'index'})
+                })
+            }
+        }
+    }
 </script>
 
 <style>
