b3346ebdd4e09057a1335baa2cf29266dd5ccd06
zhangbaicheng <zhangbaicheng@sinosoft.com.cn>
2020-10-23 15:51:56 +0800

    添加footer

---
+++ src/components/footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item name="search" icon="search">标签</van-tabbar-item>
+      <van-tabbar-item name="friends" icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      active: "home",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/layout.vue
+++ src/views/layout.vue
@@ -2,10 +2,18 @@
   <div>
     主布局
     <router-view />
+    <app-footer />
   </div>
 </template>
 
 <script>
+// 此处引入名字避免使用H5标签名footer，否则会引入失败
+import AppFooter from "../components/footer";
+export default {
+  components: {
+    AppFooter
+  },
+};
 </script>
 
 <style>
